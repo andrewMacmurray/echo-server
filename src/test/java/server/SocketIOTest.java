1c1d058843adfb5a939df982b4c851ba494690e4
andrewMacmurray <a.macmurray@icloud.com>
2018-05-22 14:03:21 +0100

    testInput as instance variable

--- src/test/java/server/SocketIOTest.java
+++ src/test/java/server/SocketIOTest.java
@@ -15,12 +15,13 @@ public class SocketIOTest {
     private Socket mockSocket;
     private OutputStream outputStream;
     private InputStream inputStream;
+    private final String testInput = "hello";
 
     @Before
     public void setup() throws IOException {
         mockSocket = mock(Socket.class);
         outputStream = new ByteArrayOutputStream();
-        inputStream = new ByteArrayInputStream("test input".getBytes());
+        inputStream = new ByteArrayInputStream(testInput.getBytes());
 
         when(mockSocket.getOutputStream()).thenReturn(outputStream);
         when(mockSocket.getInputStream()).thenReturn(inputStream);
@@ -29,16 +30,16 @@ public class SocketIOTest {
     @Test
     public void writeToSocket() throws IOException {
         SocketIO socketIO = new SocketIO(mockSocket);
-        socketIO.writeToSocket("test input");
+        socketIO.writeToSocket(testInput);
 
-        assertEquals("test input", outputStream.toString().trim());
+        assertEquals(testInput, outputStream.toString().trim());
     }
 
     @Test
     public void readFomSocket() throws IOException {
         SocketIO socketIO = new SocketIO(mockSocket);
         String output = socketIO.readFromSocket();
-        assertEquals("test input", output);
+        assertEquals(testInput, output);
     }
 
 }
